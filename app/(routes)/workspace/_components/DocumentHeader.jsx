83c1e48a66c2b62f8bf67b607ab88235e363dcf7
Rishabh Das <prin100603@gmail.com>
2024-08-19 12:24:25 +0530

    added share document feature

--- app/(routes)/workspace/_components/DocumentHeader.jsx
+++ app/(routes)/workspace/_components/DocumentHeader.jsx
@@ -1,14 +1,36 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import React from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { toast } from "sonner";
 
 function DcoumentHeader() {
+	const share = useRouter();
+
+	const base = process.env.NEXT_PUBLIC_BASE_URL;
+	const pathname = usePathname();
+
+	const links = base + pathname;
+	const copylink = (e) => {
+		navigator.clipboard.writeText(links);
+		toast("Link copied to clipboard!", {
+			description:
+				"You can now share this document with anyone",
+			action: {
+				label: "Okay",
+				onClick: () => console.log("Link copied"),
+			},
+		});
+	};
+
 	return (
 		<div className="flex justify-between items-center p-3 px-7 shadow-md">
 			<div></div>
 			<OrganizationSwitcher />
 			<div className="flex gap-2">
-				<Button>Share</Button>
+				<Button onClick={copylink} >Share</Button>
 				<UserButton />
 			</div>
 		</div>
